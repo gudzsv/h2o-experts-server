04c710a6fe8b0e0b94634984c9c0fc1a28eb929f
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-09-18 19:23:33 +0200

    add auth router

---
+++ src/routers/auth.js
@@ -0,0 +1,69 @@
+import { json, Router } from 'express';
+
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { authenticate } from '../middlewares/authenticate.js';
+
+import {
+  userSchema,
+  sendResetEmailSchema,
+  resetPasswordSchema,
+  loginWithGoogleOAuthSchema,
+} from '../validation/users.js';
+import {
+  loginUserController,
+  logoutUserController,
+  refreshUserSessionController,
+  registerUserController,
+  sendResetEmailController,
+  resetPasswordController,
+  getGoogleOAuthUrlController,
+  loginWithGoogleController,
+} from '../controllers/users.js';
+
+const router = Router();
+const jsonParser = json();
+
+router.post(
+  '/register',
+  jsonParser,
+  validateBody(userSchema),
+  ctrlWrapper(registerUserController),
+);
+
+router.post(
+  '/login',
+  jsonParser,
+  validateBody(userSchema),
+  ctrlWrapper(loginUserController),
+);
+
+router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+
+router.post('/logout', ctrlWrapper(logoutUserController));
+
+router.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+router.post(
+  '/confirm-oauth',
+  validateBody(loginWithGoogleOAuthSchema),
+  ctrlWrapper(loginWithGoogleController),
+);
+
+router.use(authenticate);
+
+router.post(
+  '/send-reset-email',
+  jsonParser,
+  validateBody(sendResetEmailSchema),
+  ctrlWrapper(sendResetEmailController),
+);
+
+router.post(
+  '/reset-pwd',
+  jsonParser,
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
+
+export default router;
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import waterRouter from './water.js';
-import authRouter from './user.js';
+import authRouter from './auth.js';
+import userRouter from './user.js';
 
 import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
-router.use('/users', authRouter);
+router.use('/auth', authRouter);
+router.use('/users', userRouter);
 router.use('/water', authenticate, waterRouter);
 
 export default router;
