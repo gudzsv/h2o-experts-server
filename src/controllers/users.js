511dcb7a0dea7eb5ec8ae68502fe289d39fe7453
gudzsv <gudzsvmail@gmail.com>
2024-09-18 16:25:14 +0300

    modify session cookies params
    Co-Authored-By: OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -41,13 +41,11 @@ export const registerUserController = async (req, res) => {
 const setupSession = (res, session) => {
   res.cookie(COOKIES.REFRESH_TOKEN, session.refreshToken, {
     httpOnly: true,
-    // maxAge: THIRTY_DAYS,
-    expire: THIRTY_DAYS,
+    maxAge: THIRTY_DAYS,
   });
   res.cookie(COOKIES.SESSION_ID, session._id, {
     httpOnly: true,
     maxAge: THIRTY_DAYS,
-    // expire: THIRTY_DAYS,
   });
 };
 
